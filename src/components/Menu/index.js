de0905a2268ff3f0403456621f9ea7a83eec517f
caio-gferreira <caio_gferreira@hotmail.com>
2020-08-05 15:51:49 -0300

    react-circus

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+import Button from '../Button/Button';
+//import ButtonLink from './components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a to="/">
+                <img className="Logo" src={Logo} alt="CircusFlix logo" />
+            </a>
+        
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                Novo vídeo
+            </Button>
+        </nav>
+    )
+};
+
+export default Menu;
