5ee92f58e2c4ded023da480c8c43fb4d789198a2
caio-gferreira <caiogfferreira@gmail.com>
2024-02-27 20:36:41 -0300

    print label

--- src/components/Carousel/VideoCard/VideoCard.jsx
+++ src/components/Carousel/VideoCard/VideoCard.jsx
@@ -13,13 +13,18 @@ function getYouTubeId(youtubeURL) {
 function VideoCard({ videoTitle, videoURL, categoryColor }) {
   const image = `https://img.youtube.com/vi/${getYouTubeId(videoURL)}/hqdefault.jpg`;
   return (
-    <VideoCardContainer
-      url={image}
-      href={videoURL}
-      target="_blank"
-      style={{ borderColor: categoryColor || 'red' }}
-      title={videoTitle}
-    />
+    <div>
+      <VideoCardContainer
+        url={image}
+        href={videoURL}
+        target="_blank"
+        style={{ borderColor: categoryColor || 'red' }}
+        title={videoTitle}
+      />
+      <label>
+        {videoTitle}
+      </label>
+    </div>
   );
 }
 
